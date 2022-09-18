43531c4dbdce189593f056c36f3cf125ed725b02
RahulRanjann <rahul1913111054@gmail.com>
2022-09-18 18:19:06 +0530

    fibonacci series  using loop

--- FibonacciSequence.c
+++ FibonacciSequence.c
@@ -1,26 +1,25 @@
 #include<stdio.h>
 int fib(int n);
 int main(int argc, char const *argv[])
-{
-    fib(6);
-     
+{ 
+    printf("Enter a no. to print Fibonacci series")
+    fib(5);
     return 0;
 }
  
  int fib(int n)
  {
-        if (n == 0)
-        {
-           return 0;
-        }
-        if (n == 1)
-        {
-            return 1;
-        }
-          
-    int fibN1 = fib(n-1);
-    int fibN2 = fib(n-2);
-    int fibN = fibN1 + fibN2;
-    printf ("fib or %d is : %d \n", n, fibN);
-     return fibN;
+    int n1 = 0 ;
+    int n2 = 1;
+    int n3;
+    printf("0 \n1\n");
+    for (int i = 1; i <= n-2; i++)
+    {
+        n3 = n1+ n2;
+        printf("%d\n",n3);
+        n1 = n2;
+        n2 = n3;
+    }
+    return 0;
+
  }
