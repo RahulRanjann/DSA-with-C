45cb19aa53a96cf3c1547ee5751e74ae5ac3302f
RahulRanjann <rahul1913111054@gmail.com>
2022-09-25 18:40:47 +0530

    sum of array element

--- test.c
+++ test.c
@@ -19,20 +19,45 @@
 //   return 0;
 // }
 
-int main(int argc, char const *argv[])
+// int main(int argc, char const *argv[])
+// {
+//   int a, b;
+//   printf("Enter 1st no.");
+//   scanf("%d", &a);
+//   printf("Enter 2st no.");
+//   scanf("%d", &b);
+//   int *p, q;
+//   p = &a;
+//   q = *p;
+
+//   printf("Using point the value of a is %d \n", q);
+
+//       printf("Sum of the a and b %d", (a + b));
+
+//   return 0;
+// }
+
+
+int sum(int );
+void main(int argc, char const *argv[])
 {
-  int a, b;
-  printf("Enter 1st no.");
-  scanf("%d", &a);
-  printf("Enter 2st no.");
-  scanf("%d", &b);
-  int *p, q;
-  p = &a;
-  q = *p;
-
-  printf("Using point the value of a is %d \n", q);
-
-      printf("Sum of the a and b %d", (a + b));
-
-  return 0;
+ printf("%d\n",sum(6));
+  
 }
+
+int sum(int n)
+{
+ 
+  if (n==0)
+  {
+    
+    return 0;
+  }
+   int a = n;
+  printf("%d ",a);
+  sum(n-1);
+
+ 
+ 
+}
+
